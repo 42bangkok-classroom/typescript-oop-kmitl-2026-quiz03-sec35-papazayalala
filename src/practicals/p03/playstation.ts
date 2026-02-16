8bc029dbdc75ec16e32479982ea41a3ccd354c8d
supakit <68011813@kmitl.ac.th>
2026-02-16 09:58:59 +0700

    p3

--- src/practicals/p03/playstation.ts
+++ src/practicals/p03/playstation.ts
@@ -1,5 +1,21 @@
 import { Product } from './product'
 
 export class Playstation extends Product {
+  constructor(name:string, public generation:number, price:number){
+    super(name,price)
+  }
+  setPrice(price:number){
+    this.price = price;
+  }
+  getProfile(){
+    return this.name + '(Gen ' + this.generation + ')';
+  }
+  getPrice(){
+    return this.price;
+  }
+  getDiscountPrice(discount:number =1){
+    return this.price*(discount/100);
+  }
+  
+  }
   // TODO: implement class properties, constructor with super(...), and methods
-}
--- src/practicals/p03/product.ts
+++ src/practicals/p03/product.ts
@@ -1,2 +1,17 @@
-export class Product {}
+export class Product {
+    name: string;
+    price:number;
+
+    constructor(name: string, price: number=0){
+        this.name = name;
+        this.price = price;
+    }
+    setPrice(price: number){
+        this.price = price;
+    }
+    getPrice(){
+        return this.price;
+    }
+
+}
 
