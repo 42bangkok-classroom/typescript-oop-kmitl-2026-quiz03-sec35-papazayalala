7299fde2c45d4b2c8ba066d10ef65929cf1b4bd6
supakit <68011813@kmitl.ac.th>
2026-02-16 10:13:18 +0700

    p4

--- src/practicals/p04/user.ts
+++ src/practicals/p04/user.ts
@@ -1,1 +1,20 @@
-export class User {}
+export class User {
+    constructor(public username: string, private password:string){
+        this.username = username;
+        this.password = password;
+
+    }
+    private loginAttempts = 0;
+    login(password:string): boolean{
+        if(password === this.password){
+            return true;
+        }
+        else{
+            return false;
+        }
+        this.loginAttempts++;
+    }
+    getLoginAttempts(){
+        return this.loginAttempts;
+    }
+}
