824c9382dd7014292f39ec72099d45e20efbb401
supakit <68011813@kmitl.ac.th>
2026-02-16 10:30:05 +0700

    i

--- src/practicals/p02/person.ts
+++ src/practicals/p02/person.ts
@@ -1,11 +1,10 @@
 export class Person {
     firstname?: string;
     lastname?: string;
-    private age?: number;
-
-    static COUNTRY = 'thailand'
-    setAge(age?: number){
-        this.age = age;
+    private age: number;
+    static COUNTRY = 'Thailand'
+    setAge(n: number){
+        this.age = n;
     }
     getFullName(){
         return this.firstname + ' ' + this.lastname;
